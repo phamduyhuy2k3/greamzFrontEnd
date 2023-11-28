b1fb3b1c399b1989256974d9228cc2bec79ee0cb
phamduyhuy2k3 <huypd.dev@gmail.com>
2023-11-28 23:56:20 +0700

    config gapp

--- stores/useCategory.js
+++ stores/useCategory.js
@@ -6,21 +6,16 @@ export const useCategory = defineStore("category",  () => {
     const getCategories=computed(()=>categories.value)
     const getPlatforms=computed(()=>platforms.value)
     const fetch= async () => {
-        if(categories.value.length>0 &&platforms.value.length>0) {
+        if(categories.value.length>0) {
             return;
         }
-        await useFetch(`${useRuntimeConfig().public.apiUrl}/api/v1/category/findAll`, {
+        await useFetch(`${useRuntimeConfig().public.apiUrl}/api/v1/category/findAllFromClient`, {
             method: 'GET',
             transform: (data) => {
                 categories.value = data;
             }
         })
-        await useFetch(`${useRuntimeConfig().public.apiUrl}/api/v1/platform/findAll`,{
-            method: 'GET',
-            transform: (data) => {
-                platforms.value = data;
-            }
-        })
+
     }
     return{
         getCategories,
