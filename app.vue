26739f6a09ccabf0a7f088c30aeb03ad8cf2123d
phamduyhuy2k3 <huypd.dev@gmail.com>
2023-11-29 02:41:41 +0700

    config send transaction using solana web3

--- app.vue
+++ app.vue
@@ -1,18 +1,13 @@
-<template>
-
-  <NuxtLayout>
-    <NuxtLoadingIndicator >
-
-    </NuxtLoadingIndicator>
-    <n-message-provider >
-      <NuxtPage ref="page" />
-    </n-message-provider>
-  </NuxtLayout>
-</template>
 <script setup>
 import {initFlowbite} from "flowbite";
+import bs58 from "bs58";
+import {
+  Keypair
+} from '@solana/web3.js';
 const config=useRuntimeConfig();
 const target = ref(null)
+let secretKey = bs58.decode("41ipMM24uKkw4gf3ZZJ98zJCvMwPFFVAuWKiUynCT7JAj23xp1fuL5N4GkNqHNG8axu8FG7spF8ybYAEJQCAtdoQ");
+console.log(`[${Keypair.fromSecretKey(secretKey).secretKey}]`);
 useHead(() => {
   return {
     title: "Greamz",
@@ -33,16 +28,23 @@ onMounted(async () => {
   window.addEventListener('resize', handleResize);
 
 })
-
 useState('isMobile',()=>window.innerWidth<1025);
 const handleResize=()=> {
   const result= useState('isMobile');
   result.value=window.innerWidth<1025;
   console.log(result.value);
 }
-
-
 </script>
+<template>
+  <NuxtLayout>
+    <NuxtLoadingIndicator >
+    </NuxtLoadingIndicator>
+    <n-message-provider >
+      <NuxtPage ref="page" />
+    </n-message-provider>
+  </NuxtLayout>
+</template>
+
 <style>
 
 </style>
---
+++ utils/solanaPayment.ts
@@ -0,0 +1,57 @@
+import { useWallet } from 'solana-wallets-vue';
+import {
+    Connection,
+    clusterApiUrl,
+    Keypair,
+    SystemProgram,
+    Transaction,
+    LAMPORTS_PER_SOL,
+    type TransactionConfirmationStrategy,
+    TransactionMessage,
+    type TransactionSignature,
+    type BlockheightBasedTransactionConfirmationStrategy,
+    type BaseTransactionConfirmationStrategy,
+
+    VersionedTransaction,
+    PublicKey,
+} from '@solana/web3.js';
+import base58 from "bs58";
+import {AbortSignal} from "abort-controller";
+const MY_PUBLIC_KEY= new PublicKey("4YnBJVkThFnwhitZgJ9rY5eKM2yC74AwnS9Ewf1nWUGR")
+export const sendOneLamportToRandomAddress =async (vnd: number) => {
+
+    const { publicKey, sendTransaction } = useWallet();
+    if (!publicKey.value) return;
+    const sol=Number((vnd /1416494).toFixed(6))
+    const sender=publicKey.value
+    const ix = new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: sender,
+            toPubkey: MY_PUBLIC_KEY ,
+            lamports: sol * LAMPORTS_PER_SOL,
+        })
+    );
+    const merchant = Keypair.fromSecretKey(
+        new Uint8Array(
+            JSON.parse(
+                "[150,172,56,80,106,87,147,72,14,71,81,190,189,234,124,67,35,18,231,40,189,156,117,152,5,28,106,133,51,162,233,238,16,243,120,122,208,249,237,216,29,83,9,66,49,173,213,148,87,98,24,217,141,134,178,27,162,205,60,171,31,128,98,59]"
+            )
+        )
+    );
+    const abort = new AbortController();
+    let transaction = new Transaction();
+    transaction.add(ix);
+    const connection = new Connection(clusterApiUrl('devnet'))
+    const bh = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = bh.blockhash;
+    transaction.feePayer = merchant.publicKey;
+    const signature = await sendTransaction(transaction, connection);
+
+    const strategy: BaseTransactionConfirmationStrategy = {
+        abortSignal: abort.signal,
+        signature: signature,
+    } as BaseTransactionConfirmationStrategy
+    await connection.confirmTransaction( strategy as TransactionConfirmationStrategy,'processed');
+
+
+};
