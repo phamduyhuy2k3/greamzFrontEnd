d02b234c535e9c4fb865f119af9e6f54c567ef69
phamduyhuy2k3 <huypd.dev@gmail.com>
2023-11-29 02:41:41 +0700

    config send transaction using solana web3

--- app.vue
+++ app.vue
@@ -1,16 +1,6 @@
-<template>
-
-  <NuxtLayout>
-    <NuxtLoadingIndicator >
-
-    </NuxtLoadingIndicator>
-    <n-message-provider >
-      <NuxtPage ref="page" />
-    </n-message-provider>
-  </NuxtLayout>
-</template>
 <script setup>
 import {initFlowbite} from "flowbite";
+
 const config=useRuntimeConfig();
 const target = ref(null)
 useHead(() => {
@@ -33,16 +23,23 @@ onMounted(async () => {
   window.addEventListener('resize', handleResize);
 
 })
-
 useState('isMobile',()=>window.innerWidth<1025);
 const handleResize=()=> {
   const result= useState('isMobile');
   result.value=window.innerWidth<1025;
   console.log(result.value);
 }
-
-
 </script>
+<template>
+  <NuxtLayout>
+    <NuxtLoadingIndicator >
+    </NuxtLoadingIndicator>
+    <n-message-provider >
+      <NuxtPage ref="page" />
+    </n-message-provider>
+  </NuxtLayout>
+</template>
+
 <style>
 
 </style>
--- plugins/solana.ts
+++ plugins/solana.ts
@@ -1,9 +1,10 @@
 import "solana-wallets-vue/styles.css";
 import SolanaWallets from "solana-wallets-vue";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {WalletAdapterNetwork, WalletError,} from "@solana/wallet-adapter-base";
 import {
     PhantomWalletAdapter,
     SolflareWalletAdapter,
+
 } from "@solana/wallet-adapter-wallets";
 import {defineNuxtPlugin} from "#app";
 
@@ -13,7 +14,11 @@ const walletOptions = {
 
         new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
     ],
+    cluster: WalletAdapterNetwork.Devnet,
     autoConnect: true,
+    onError: (error: WalletError) => {
+        console.error(error);
+    }
 };
 
 export default defineNuxtPlugin((nuxtApp) => {
